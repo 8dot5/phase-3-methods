dab35d561fc58755d50dff338873ec497d11b977
Chun Yi <8.5by11@gmail.com>
2022-02-23 00:07:37 -0500

    commit tree 822cb3661ef6d3fde0fdd09ae9a1fc93d003372f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(2,5)
+
+
+def halve(num1)
+    if num1 == 6
+        return num1 / 2
+    else
+        return nil
+    end
+end
+
